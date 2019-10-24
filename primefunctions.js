2123deac5487d3c2673a89cd540a263d0b80d112
Jakobminer <jakobminer25@gmail.com>
2019-10-23 21:45:29 -0700

    finished maxPrimeSum

--- primefunctions.js
+++ primefunctions.js
@@ -40,3 +40,30 @@ function cumulativeSum(nums) {
 }
 
 console.log(cumulativeSum([1, 2, 3, 4]));
+
+function maxPrimeSum(num) {
+  const allPrimes = primeGen(num);
+  let i;
+  let j;
+  let primeSum;
+  let length;
+  let maxSum;
+  let maxLength = 0;
+  for (i = 0; i < allPrimes.length; i++) {
+    primeSum = allPrimes[i];
+    length = 1;
+    for (j = i + 1; j < allPrimes.length; j++) {
+      primeSum += allPrimes[j];
+      length++;
+      if (isPrime(primeSum) && primeSum < num) {
+        if (length > maxLength) {
+          maxSum = primeSum;
+          maxLength = length;
+        }
+      }
+    }
+  }
+  return [maxSum, maxLength];
+}
+
+console.log(maxPrimeSum(1000));
