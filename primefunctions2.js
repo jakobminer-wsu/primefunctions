a3bad7bc808ec39b82086e13dac3b3e55bac0622
Jakobminer <jakobminer25@gmail.com>
2019-12-03 18:57:42 -0800

    improved isprime/primegen

---
+++ primefunctions2.js
@@ -0,0 +1,77 @@
+function isPrime(num) {
+  if (num % 2 === 0) return (num === 2);
+  if (num % 3 === 0) return (num === 3);
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0) return false;
+    if (num % (i + 2) === 0) return false;
+  }
+  return true;
+}
+
+function primeGen(num) {
+  const primes = [];
+  const allNums = [];
+  for (let i = 0; i < num; i++) {
+    allNums.push(true);
+  }
+  for (let i = 2; i * i <= num; i++) {
+    if (allNums[i]) {
+      for (let j = i * i; j < num; j += i) {
+        allNums[j] = false;
+      }
+    }
+  }
+  for (let i = 2; i < num; i++) {
+    if (allNums[i]) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
+
+console.log(primeGen(10));
+
+function cumulativeSum(nums) {
+  const sums = [];
+  let tempSum;
+  let i;
+  let j;
+  for (i = 0; i < nums.length; i++) {
+    tempSum = 0;
+    for (j = 0; j <= i; j++) {
+      tempSum += nums[j];
+    }
+    sums.push(tempSum);
+  }
+  return sums;
+}
+
+console.log(cumulativeSum([1, 2, 3, 4]));
+
+function maxPrimeSum(num) {
+  const allPrimes = primeGen(num);
+  let i;
+  let j;
+  let primeSum;
+  let length;
+  let maxSum;
+  let maxLength = 0;
+  for (i = 0; i < allPrimes.length; i++) {
+    primeSum = allPrimes[i];
+    length = 1;
+    for (j = i + 1; j < allPrimes.length; j++) {
+      primeSum += allPrimes[j];
+      length++;
+      if (isPrime(primeSum) && primeSum < num) {
+        if (length > maxLength) {
+          maxSum = primeSum;
+          maxLength = length;
+        }
+      }
+    }
+  }
+  return [maxSum, maxLength];
+}
+
+console.log(maxPrimeSum(100));
+console.log(maxPrimeSum(1000));
---
+++ test/testPerformance.js
@@ -0,0 +1,17 @@
+/* eslint-env mocha, chai */
+/* global maxPrimeSum, chai */
+
+describe('Test for Performance', function () {
+  describe('maxPrimeSum', function () {
+    it('maxPrimeSum(10000)should take less than 20ms', function () {
+      this.slow(0);
+      this.timeout(20);
+      chai.assert.deepEqual(maxPrimeSum(10000), [9521, 65]);
+    });
+    it('maxPrimeSum(100000) should take less than 200ms', function () {
+      this.slow(0);
+      this.timeout(200);
+      chai.assert.deepEqual(maxPrimeSum(100000), [92951, 183]);
+    });
+  });
+});
